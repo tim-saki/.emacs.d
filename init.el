a838af7009f0d9778a81c66837c1e0e51c37ae9e
tim-saki <tim-saki>
2013-09-10 10:42:20 +0900

    add coffee-mode

--- init.el
+++ init.el
@@ -56,6 +56,7 @@
 	 helm
 	 auto-complete
 	 markdown-mode
+	 coffee-mode
 	 )))
 
 (el-get 'sync my-el-get-packages)
@@ -82,4 +83,12 @@
   (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
   (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
   (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+  )
+
+(when (require 'coffee-mode nil t)
+  (defun coffee-custom ()
+    "coffee-mode-hook"
+    (and (set (make-local-variable 'tab-width) 2)
+	 (set (make-local-variable 'coffee-tab-width) 2)))
+  (add-hook 'coffee-mode-hook '(lambda() (coffee-custom)))
   )
